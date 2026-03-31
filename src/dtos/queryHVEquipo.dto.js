70dbbc881a2339463b94eea4585656729ec8b8f2
MARTIN DAVID DURAN MARTINEZ <102440299+tintindavid@users.noreply.github.com>
2026-03-30 22:52:29 -0500

    limitCustomer

--- src/dtos/queryHVEquipo.dto.js
+++ src/dtos/queryHVEquipo.dto.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const queryHVEquipoDto = Joi.object({
   page: Joi.number().integer().min(1).default(1),
-  limit: Joi.number().integer().min(1).max(100).default(10),
+  limit: Joi.number().integer().min(1).max(100).default(200),
   sortBy: Joi.string().default('createdAt'),
   order: Joi.string().valid('asc','desc').default('desc'),
   search: Joi.string().optional(),
